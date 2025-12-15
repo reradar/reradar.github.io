a4d1e0496df0224f93b1e5e30ede9489514ef96b
reradar <165357523+reradar@users.noreply.github.com>
2025-12-15 15:58:28 -0800

    Update data

--- scripts/update_rewards.py
+++ scripts/update_rewards.py
@@ -19,20 +19,20 @@ class Worker:
             return []
         soup = BeautifulSoup(resp.text, 'html.parser')
         date_div = soup.find("div", class_ = "dividend-quarterly-offer-content__offer__labels__signup-label")
-        signup_date = datetime.strptime(date_div.span.contents[1].string, "%m/%d/%Y").date()
+        signup_date = datetime.strptime(date_div.span.contents[2].string, "%m/%d/%Y").date()
         next_q_date_int = self.get_next_quarter_start(signup_date)
         if next_q_date_int != target_date_int:
             return []
         divs = soup.find_all("div", class_ = "dividend-quarterly-offer-content__offer__labels__title")
         return [self.process_item(div.h3.contents[0]) for div in divs]
 
-    def get_chase_rewards_bak(self, target_date_int: int) -> list[str]:
+    def get_chase_rewards(self, target_date_int: int) -> list[str]:
         resp = requests.get("https://www.chase.com/personal/credit-cards/freedom/freedomfive")
         if resp.status_code != 200:
             return []
         soup = BeautifulSoup(resp.text, 'html.parser')
         date_range = soup.find("span", class_ = "mds-body-large")
-        match = re.search(r"From (\w+ \d{1,2}) - \w+ \d{1,2}, (\d{4})", date_range.contents[0])
+        match = re.search(r"From (\w+ \d{1,2}), (\d{4}) through \w+ \d{1,2}, \d{4}", date_range.contents[0])
         date_integer = 0
         if match:
             month_day = match.group(1)  # "October 1"
@@ -44,7 +44,7 @@ class Worker:
             return [self.process_item(span.contents[0]) for span in spans]
         return []
     
-    def get_chase_rewards(self, target_date_int: int) -> list[str]:
+    def get_chase_rewards_1(self, target_date_int: int) -> list[str]:
         resp = requests.get("https://www.chasebonus.com/")
         if resp.status_code != 200:
             return []
